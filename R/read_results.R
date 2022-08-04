92b8a7fd4be6f9dc90c62dac1dbebb65bfb108bd
Lucas <lucasdsveras@gmail.com>
2022-08-04 10:45:13 +0100

    Remove duplicated code

--- R/read_results.R
+++ R/read_results.R
@@ -1,69 +1,51 @@
 read_results <- function(url, round, session) {
-  if (grepl("FP", session)) {
+  if (grepl("FP", session, ignore.case = TRUE)) {
     session2 <- paste0("practice-", substr(session, 3, 3))
     url2 <- paste0(url, session2, ".html")
+  } else if (grepl("quali", session, ignore.case = TRUE)) {
+    session <- "Qualifying"
+    session2 <- "starting-grid"
+    url2 <- paste0(url, session2, ".html")
+  } else if (grepl("race", session, ignore.case = TRUE)) {
+    session <- "Race"
+    session2 <- "race-result"
+    url2 <- paste0(url, session2, ".html")
+  }
 
-    results <- rvest::read_html(url2)
-    results <- rvest::html_element(results, "body")
-    results <- rvest::html_table(results)
+  results <- rvest::read_html(url2)
+  results <- rvest::html_element(results, "body")
+  results <- rvest::html_table(results)
+
+  if (grepl("FP", session, ignore.case = TRUE)) {
     results <- dplyr::select(
       results,
       position = Pos, driver = Driver, team = Car,
       time = Time, gap = Gap, laps = Laps
     )
-    results <- format_data(results)
-    results <- dplyr::mutate(
-      results,
-      season = 2022,
-      round = round,
-      session = session,
-      .before = 1
-    )
-  } else if (grepl("quali", session)) {
-    session <- "Qualifying"
-    session2 <- "starting-grid"
-    url2 <- paste0(url, session2, ".html")
-
-    results <- rvest::read_html(url2)
-    results <- rvest::html_element(results, "body")
-    results <- rvest::html_table(results)
+  } else if (grepl("quali", session, ignore.case = TRUE)) {
     results <- dplyr::select(
       results,
       position = Pos, driver = Driver, team = Car, time = Time
     )
-    results <- format_data(results)
-    results <- dplyr::mutate(
-      results,
-      season = 2022,
-      round = round,
-      session = session,
-      .before = 1
-    )
-  } else if (grepl("race", session)) {
-    session <- "Race"
-    session2 <- "race-result"
-    url2 <- paste0(url, session2, ".html")
-
-    results <- rvest::read_html(url2)
-    results <- rvest::html_element(results, "body")
-    results <- rvest::html_table(results)
+  } else if (grepl("race", session, ignore.case = TRUE)) {
     results <- dplyr::select(
       results,
       position = Pos, driver = Driver, team = Car,
       laps = Laps, time = `Time/Retired`, points = PTS
     )
-    results <- format_data(results)
-    results <- dplyr::mutate(
-      results,
-      season = 2022,
-      round = round,
-      session = session,
-      position = ifelse(
-        time == "DNF", "DNF", position
-      ),
-      .before = 1
-    )
   }
+
+  results <- format_data(results)
+  results <- dplyr::mutate(
+    results,
+    season = 2022,
+    round = round,
+    session = session,
+    position = ifelse(
+      time == "DNF", "DNF", position
+    ),
+    .before = 1
+  )
 
   results
 
