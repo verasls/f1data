0a7669e4c6c5e4ef8b7086fba091c1e6764e60af
Lucas <lucasdsveras@gmail.com>
2022-08-04 10:26:16 +0100

    Remove the pipe operator

--- R/read_results.R
+++ R/read_results.R
@@ -3,60 +3,66 @@ read_results <- function(url, round, session) {
     session2 <- paste0("practice-", substr(session, 3, 3))
     url2 <- paste0(url, session2, ".html")
 
-    results <- rvest::read_html(url2) |>
-      rvest::html_element("body") |>
-      rvest::html_table() |>
-      dplyr::select(
-        position = Pos, driver = Driver, team = Car,
-        time = Time, gap = Gap, laps = Laps
-      ) |>
-      format_data() |>
-      dplyr::mutate(
-        season = 2022,
-        round = round,
-        session = session,
-        .before = 1
-      )
+    results <- rvest::read_html(url2)
+    results <- rvest::html_element(results, "body")
+    results <- rvest::html_table(results)
+    results <- dplyr::select(
+      results,
+      position = Pos, driver = Driver, team = Car,
+      time = Time, gap = Gap, laps = Laps
+    )
+    results <- format_data(results)
+    results <- dplyr::mutate(
+      results,
+      season = 2022,
+      round = round,
+      session = session,
+      .before = 1
+    )
   } else if (grepl("quali", session)) {
     session <- "Qualifying"
     session2 <- "starting-grid"
     url2 <- paste0(url, session2, ".html")
 
-    results <- rvest::read_html(url2) |>
-      rvest::html_element("body") |>
-      rvest::html_table() |>
-      dplyr::select(
-        position = Pos, driver = Driver, team = Car, time = Time
-      ) |>
-      format_data() |>
-      dplyr::mutate(
-        season = 2022,
-        round = round,
-        session = session,
-        .before = 1
-      )
+    results <- rvest::read_html(url2)
+    results <- rvest::html_element(results, "body")
+    results <- rvest::html_table(results)
+    results <- dplyr::select(
+      results,
+      position = Pos, driver = Driver, team = Car, time = Time
+    )
+    results <- format_data(results)
+    results <- dplyr::mutate(
+      results,
+      season = 2022,
+      round = round,
+      session = session,
+      .before = 1
+    )
   } else if (grepl("race", session)) {
     session <- "Race"
     session2 <- "race-result"
     url2 <- paste0(url, session2, ".html")
 
-    results <- rvest::read_html(url2) |>
-      rvest::html_element("body") |>
-      rvest::html_table() |>
-      dplyr::select(
-        position = Pos, driver = Driver, team = Car,
-        laps = Laps, time = `Time/Retired`, points = PTS
-      ) |>
-      format_data() |>
-      dplyr::mutate(
-        season = 2022,
-        round = round,
-        session = session,
-        position = ifelse(
-          time == "DNF", "DNF", position
-        ),
-        .before = 1
-      )
+    results <- rvest::read_html(url2)
+    results <- rvest::html_element(results, "body")
+    results <- rvest::html_table(results)
+    results <- dplyr::select(
+      results,
+      position = Pos, driver = Driver, team = Car,
+      laps = Laps, time = `Time/Retired`, points = PTS
+    )
+    results <- format_data(results)
+    results <- dplyr::mutate(
+      results,
+      season = 2022,
+      round = round,
+      session = session,
+      position = ifelse(
+        time == "DNF", "DNF", position
+      ),
+      .before = 1
+    )
   }
 
   results
