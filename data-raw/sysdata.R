90770cd5671825889a67e1da655543062ff7c458
Lucas <lucasdsveras@gmail.com>
2022-08-07 16:53:02 +0100

    Add schedule data

--- data-raw/sysdata.R
+++ data-raw/sysdata.R
@@ -2,5 +2,7 @@
 urls <- readr::read_csv("data-raw/urls.csv")
 # sprint dataset
 sprint <- readr::read_csv("data-raw/sprint.csv")
+# schedule dataset
+schedule <- readr::read_csv("data-raw/schedule.csv")
 
-usethis::use_data(urls, sprint, overwrite = TRUE, internal = TRUE)
+usethis::use_data(urls, sprint, schedule, overwrite = TRUE, internal = TRUE)
