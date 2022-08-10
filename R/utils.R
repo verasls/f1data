e16257ecef3300a0cd4677822838bdda44ab099e
Lucas <lucasdsveras@gmail.com>
2022-08-10 17:47:28 +0100

    Add get_f1_ulrs() function

---
+++ R/utils.R
@@ -0,0 +1,110 @@
+#' @importFrom rlang .data
+get_schedule <- function(season) {
+  url <- "http://ergast.com/api/f1/"
+  url <- paste0(url, season, ".json")
+
+  schedule <- jsonlite::fromJSON(httr::content(httr::GET(url), as = "text"))
+  schedule <- tibble::as_tibble(schedule$MRData$RaceTable$Races)
+  schedule <- tidyr::unnest(schedule, .data$Circuit, names_sep = "_")
+  schedule <- dplyr::select(
+    schedule,
+    season, round_num = round, round_name = .data$raceName,
+    circuit = .data$Circuit_circuitName, date
+  )
+  schedule <- dplyr::mutate(
+    schedule,
+    season = as.numeric(.data$season),
+    round_num = as.numeric(.data$round_num),
+    date = as.Date(date)
+  )
+  schedule
+}
+
+get_f1_urls <- function(season) {
+  home <- "https://www.formula1.com"
+  url <- glue::glue(
+    "https://www.formula1.com/en/results.html/{season}/races.html"
+  )
+
+  urls <- rvest::read_html(url)
+  urls <- rvest::html_element(urls, "body")
+  urls <- rvest::html_elements(urls, "a")
+  urls <- as.character(urls)
+
+  i <- which(grepl("(.*2022)(.*race-result.html)", urls))
+  urls <- urls[i]
+  urls <- strsplit(urls, split = "\\\"")
+  urls <- purrr::map_chr(urls, 2)
+
+  duplicated <- data.frame(table(urls))
+  duplicated <- as.character(duplicated$urls[duplicated$Freq > 1])
+  to_remove <- purrr::map_dbl(duplicated, ~ which(urls == .x)[2])
+  urls <- urls[- to_remove]
+
+  urls <- paste0(home, urls)
+  urls <- substr(urls, 1, nchar(urls) - 16)
+
+  urls_df <- tibble::tibble(
+    season = season, round_num = seq_along(urls), urls = urls
+  )
+
+  check_f1_urls(season, urls_df)
+  urls_df
+}
+
+check_f1_urls <- function(season, urls_df) {
+  url <- glue::glue(
+    "https://www.formula1.com/en/results.html/{season}/races.html"
+  )
+
+  urls <- rvest::read_html(url)
+  urls <- rvest::html_element(urls, "body")
+  urls <- as.character(urls)
+
+  i <- stringi::stri_locate_first(urls, fixed = "raceNavigationItems")[1]
+  urls <- substr(urls, i, nchar(urls))
+  i <- stringi::stri_locate_first(urls, fixed = "]")[1]
+  urls <- substr(urls, 1, 6755)
+
+  i <- stringi::stri_locate_all(
+    urls,
+    regex = "\"meetingKey\":[:digit:]+,"
+  )
+  start_i <- i[[1]][, 1]
+  end_i <- i[[1]][, 2]
+  key <- purrr::map2_chr(start_i, end_i, ~ substr(urls, .x, .y))
+  key <- substr(key, 14, nchar(key) - 1)
+
+  i <- stringi::stri_locate_all(
+    urls,
+    regex = "\"meetingNumber\":[:digit:]+,"
+  )
+  start_i <- i[[1]][, 1]
+  end_i <- i[[1]][, 2]
+  num <- purrr::map2_chr(start_i, end_i, ~ substr(urls, .x, .y))
+  num <- as.numeric(substr(num, 17, nchar(num) - 1))
+
+  check_df <- tibble::tibble(num = num, key = key)
+  check_df <- dplyr::arrange(check_df, num)
+
+  if (any(diff(check_df$num) != 1)) {
+    rlang::abort("Wrong sequence of round numbers")
+  }
+
+  check_df <- tibble::add_column(urls_df, check_df)
+
+  num_ok <- ifelse(check_df$round_num - check_df$num != 0, FALSE, TRUE)
+
+  key_ok <- substr(urls_df$urls, 53, nchar(urls_df$urls))
+  i <- stringi::stri_locate_first(key_ok, fixed = "/")[, 1]
+  key_ok <- substr(key_ok, 1, i - 1)
+  key_ok <- purrr::map_lgl(
+    seq_along(key_ok), ~ grepl(key_ok[.x], check_df$urls[.x])
+  )
+
+  if (any(isFALSE(num_ok)) || any(isFALSE(key_ok))) {
+    rlang::abort("The urls do not match the round numbers")
+  } else {
+    NULL
+  }
+}
