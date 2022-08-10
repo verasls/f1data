5d47040e6befb3a97842533123fa49ff4a434283
Lucas <lucasdsveras@gmail.com>
2022-08-10 22:33:04 +0100

    Delete files

--- data-raw/sysdata.R
+++
@@ -1,8 +0,0 @@
-# urls dataset
-urls <- readr::read_csv("data-raw/urls.csv")
-# sprint dataset
-sprint <- readr::read_csv("data-raw/sprint.csv")
-# schedule dataset
-schedule <- readr::read_csv("data-raw/schedule.csv")
-
-usethis::use_data(urls, sprint, schedule, overwrite = TRUE, internal = TRUE)
